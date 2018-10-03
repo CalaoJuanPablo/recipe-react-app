ef7032f345e2f69fdecb69925e765f18c14e33d1
Juan Pablo Calao Perez <jcalaop@gmail.com>
2018-10-03 13:23:58 -0500

    Recipe Card MOdule

---
+++ src/components/Recipe/Recipe.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./Recipe.css"
+
+export default class Recipe extends Component {
+  render() {
+    const {title, instructions, img} = this.props;
+    const ingredients = this.props.ingredients.map((ing, index) => {
+      return <li key={index}>{ing}</li>;
+    })
+    return (
+      <div className="recipe-card">
+        <div className="recipe-card-image">
+          <img src={img} alt={title} />
+        </div>
+        <div className="recipe-card-content">
+          <h3 className="recipe-card-title">{title}</h3>
+          <h4>Ingredients:</h4>
+          <ul>
+            {ingredients}
+          </ul>
+          <h4>Instructions:</h4>
+          <p>{instructions}</p>
+        </div>
+      </div>
+    )
+  }
+}
