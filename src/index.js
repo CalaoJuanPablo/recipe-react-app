141c795c29789dffdfff6f4093d33fd9c8814f5e
Juan Pablo Calao Perez <jcalaop@gmail.com>
2018-10-19 12:56:04 -0500

    Integrating firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCYUEmu5vOXUeiUUpueCYzb1jxJOqUIXdg",
+  authDomain: "recipeapp-5741e.firebaseapp.com",
+  databaseURL: "https://recipeapp-5741e.firebaseio.com",
+  projectId: "recipeapp-5741e",
+  storageBucket: "",
+  messagingSenderId: "895322652070"
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
