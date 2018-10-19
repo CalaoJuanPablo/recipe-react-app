95d3725d1d736ba6319ac342ecae406bc7cf3b47
Juan Pablo Calao Perez <jcalaop@gmail.com>
2018-10-19 13:45:15 -0500

    Adding source code repo link

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,6 +11,7 @@ export default class NavBar extends Component {
           <li><a href="#">Home</a></li>
           <li><a href="#">About</a></li>
           <li><a href="#">Contanct Us</a></li>
+          <li><a href="https://github.com/CalaoJuanPablo/recipe-react-app" target="_blank">GitHub Repo to this project</a></li>
         </nav>
       </header>
     );
